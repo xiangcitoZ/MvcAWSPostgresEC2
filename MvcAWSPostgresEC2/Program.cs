45036bf29a733d6921281011fc674777b0982905
xiangcitoZ <pzxiang1@gmail.com>
2023-05-23 13:07:34 +0200

    d

--- MvcAWSPostgresEC2/Program.cs
+++ MvcAWSPostgresEC2/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using MvcAWSPostgresEC2.Data;
+using MvcAWSPostgresEC2.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string connectionString =
+    builder.Configuration.GetConnectionString("Postgres");
+builder.Services.AddTransient<RepositoryDepartamentos>();
+builder.Services.AddDbContext<DepartamentosContext>
+    (options => options.UseNpgsql(connectionString));
+
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
